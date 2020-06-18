7a4b8982c2f7752c80ffa4bfb94881dcce6d3168
mohamed525 <mohamedabdelazeem513@gmail.com>
2020-06-18 23:36:51 +0200

    untested release with 6 pins in arduino

--- sketch_jun12a/sketch_jun12a.ino
+++ sketch_jun12a/sketch_jun12a.ino
@@ -4,13 +4,22 @@
 #define shift_data_2 6
 #define shift_clk_2  5
 #define st_reg_clk_2 7  
+#define button 12
+
+
+
+
 
 byte leds ;
 byte leds2 ;
+int speed = 100;
+
+
+void game_mode(int speed);
 void update_Storage_register();
-void game_mode();
+void chasing_led();
 void winner_mode();
-void 
+void lose_mode();
 
 
 void setup() {
@@ -20,9 +29,28 @@ void setup() {
   pinMode(shift_data_2, OUTPUT);
   pinMode(shift_clk_2, OUTPUT);
   pinMode(st_reg_clk_2, OUTPUT);
+  pinMode(button, INPUT);
+  
 }
 
 void loop() {
+  game_mode(speed);
+
+ }
+
+void update_Storage_register(){
+  shiftOut(shift_data, shift_clk, LSBFIRST, leds);
+  digitalWrite(st_reg_clk, LOW);
+  digitalWrite(st_reg_clk, HIGH);
+  digitalWrite(st_reg_clk, LOW);
+  shiftOut(shift_data_2, shift_clk_2, LSBFIRST, leds2);
+  digitalWrite(st_reg_clk_2, LOW);
+  digitalWrite(st_reg_clk_2, HIGH);
+  digitalWrite(st_reg_clk_2, LOW);
+}
+
+
+void game_mode(int speed){
   leds = 0;
   leds2 = 0;
   update_Storage_register();
@@ -33,31 +61,62 @@ void loop() {
       if(i != 0){
       leds &= ~(1<<(i-1));
       }
+      if (digitalRead(button) == 0 && leds == 0b00000001)
+      {
+        winner_mode();
+        speed -= 20;
+      }else if(digitalRead(button) == 0 && leds != 0b00000001){
+        lose_mode();
+        speed = 100;
+      }
       update_Storage_register();
-      delay(10);
+      delay(speed);
   }
   leds = 0;
   update_Storage_register();
   for (int i = 0; i < 8; i++)  // Turn all the LEDs ON one by one.
   {
-  
       bitSet(leds2, i); // Set the bit that controls that LED in the variable 'leds'
       update_Storage_register();
       if(i != 0){
       leds2 &= ~(1<<(i-1));
       }
+      if(digitalRead(button) == 0 && leds2 != 0b00000001){
+        lose_mode();
+        speed = 100;
+      }
       update_Storage_register();
-      delay(10);
+      delay(speed);
   }  
 }
 
-void update_Storage_register(){
-  shiftOut(shift_data, shift_clk, MSBFIRST, leds);
-  digitalWrite(st_reg_clk, LOW);
-  digitalWrite(st_reg_clk, HIGH);
-  digitalWrite(st_reg_clk, LOW);
-  shiftOut(shift_data_2, shift_clk_2, MSBFIRST, leds2);
-  digitalWrite(st_reg_clk_2, LOW);
-  digitalWrite(st_reg_clk_2, HIGH);
-  digitalWrite(st_reg_clk_2, LOW);
+void winner_mode(){  
+  leds = 0;
+  leds2 = 0;
+  update_Storage_register();
+  for (int i = 0; i < 8; i++)  // Turn all the LEDs ON one by one.
+  {
+      bitSet(leds, i); // Set the bit that controls that LED in the variable 'leds'
+      update_Storage_register();
+      delay(50);
+  }
+  leds = 0;
+  update_Storage_register();
+  for (int i = 0; i < 8; i++)  // Turn all the LEDs ON one by one.
+  {
+      bitSet(leds2, i); // Set the bit that controls that LED in the variable 'leds'
+      update_Storage_register();
+      delay(50);
+  }  
+}
+
+void lose_mode(){
+    leds = 0xFF;
+    leds2 = 0xFF;
+    update_Storage_register();
+    delay(200);
+    leds = 0x00;
+    leds2 = 0x00;
+    update_Storage_register();
+     delay(250);
 }
